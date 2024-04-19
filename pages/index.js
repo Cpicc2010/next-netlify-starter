787bf504619aebc5e72df57d87070453c2880f05
Cpicc2010 <75176686+Cpicc2010@users.noreply.github.com>
2024-04-19 12:25:15 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,39 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>I nostri podcast del Lago di Fondi</title>
+</head>
+<body>
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+<header style="text-align: center; padding: 50px;">
+    <h1>I nostri podcast del Lago di Fondi</h1>
+</header>
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+<main>
+    <audio controls>
+        <source src="La Biodiversità.mp3" type="audio/mpeg">
+        Il tuo browser non supporta l'elemento audio.
+    </audio>
 
-      <Footer />
-    </div>
-  )
-}
+    <audio controls>
+        <source src="La canna di palude.mp3" type="audio/mpeg">
+        Il tuo browser non supporta l'elemento audio.
+    </audio>
+
+    <audio controls>
+        <source src="La storia del Lago di Fondi.mp3" type="audio/mpeg">
+        Il tuo browser non supporta l'elemento audio.
+    </audio>
+
+    <audio controls>
+        <source src="Uscite col Parco.mp3" type="audio/mpeg">
+        Il tuo browser non supporta l'elemento audio.
+    </audio>
+
+   
+</main>
+
+</body>
+</html>
